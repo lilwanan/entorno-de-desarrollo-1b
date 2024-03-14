fc6868077f8c55e9e4be9239e956a686c97458a0
lilwanan <juanblazer@gmail.com>
2024-03-14 14:00:41 +0100

    Eclipse prueba

---
+++ Personas/src/modelos/Persona.java
@@ -0,0 +1,67 @@
+package modelos;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    private int edad = 0;
+
+    /**
+     * @param nombre 
+     * @param apellido1 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(String nombre, String apellido1, String apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    	this.dni=dni;
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return null;
+    }
+
+}
